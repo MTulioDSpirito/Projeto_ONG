51098a1754270696df17b1f066206bb2f7618e31
Marcos S. M. Sousa <112666835+marcosabino-pucmg@users.noreply.github.com>
2024-06-03 20:45:18 -0300

    change

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$dbHost = '34.95.244.237';
-$dbUsername = 'turma3';
-$dbPassword = '123456';
+$dbHost = 'roundhouse.proxy.rlwy.net';
+$dbUsername = 'root';
+$dbPassword = 'QdbpuYyKwRQBndhIfSlCsLHlZrbiIGbe';
 $dbName = 'bd_php';
 
 $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
