0215d839f5755e551672c67f2d3b1a3f5a33cb91
MTulioDSpirito <mtdenucci@gmail.com>
2024-06-05 21:11:37 -0300

    fix editar_perfil

--- editar_perfil.php
+++ editar_perfil.php
@@ -1,4 +1,29 @@
-<?php
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="home.css">
+    <script>
+        function enableEditing() {
+            document.getElementById('nome').disabled = false;
+            document.getElementById('email').disabled = false;
+            document.getElementById('senha').disabled = false;
+            document.getElementById('endereco').disabled = false;
+            document.getElementById('salvarBtn').style.display = 'inline-block';
+            document.getElementById('editarBtn').style.display = 'none';
+        }
+    </script>
+</head>
+<body>
+    <div class="container">
+        <div class="header"></div>
+        <div class="main">
+
+        <?php
 session_start();
 if(!isset($_SESSION['user_id'])) {
     header("Location: login.php");
@@ -56,29 +81,6 @@ if ($resultado->num_rows > 0) {
 
 $conexao->close();
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="home.css">
-    <script>
-        function enableEditing() {
-            document.getElementById('nome').disabled = false;
-            document.getElementById('email').disabled = false;
-            document.getElementById('senha').disabled = false;
-            document.getElementById('endereco').disabled = false;
-            document.getElementById('salvarBtn').disabled = false;
-            document.getElementById('editarBtn').style.display = 'none';
-        }
-    </script>
-</head>
-<body>
-    <div class="container">
-        <div class="header"></div>
-        <div class="main">
-
             <div class="container">
                 <h1>Editar Perfil</h1>
                 <form method="post" action="">
@@ -99,7 +101,7 @@ $conexao->close();
                         <input type="text" class="form-control" id="endereco" name="endereco" value="<?php echo htmlspecialchars($endereco_usuario); ?>" disabled required>
                     </div>
                     <button type="button" class="btn btn-secondary" id="editarBtn" onclick="enableEditing()">Editar</button>
-                    <button type="submit" class="btn btn-primary" id="salvarBtn" disabled>Salvar</button>
+                    <button type="submit" class="btn btn-primary" id="salvarBtn" style="display: none;">Salvar</button>
                 </form>
             </div>
         </div>
