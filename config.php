44e613eb3d4c900855c5b88234a51b0219e3e499
MTulioDSpirito <mtdenucci@gmail.com>
2024-11-19 18:17:34 -0300

    up

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 $dbHost = 'autorack.proxy.rlwy.net';
 $dbUsername = 'root';
 $dbPassword = 'OaytArnxmFfaxhrHFCtsiAvysmKeHVUt';
-$dbName = 'railway';
-$dbPort = '46999';
+$dbName = 'bd_php';
+$dbPort = '44161';
 
 $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName,$dbPort);
 
